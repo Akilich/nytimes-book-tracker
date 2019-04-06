4ddc4b8fc03d974eacf82b03c72383e35a81506b
Akilich <akilihumphrey@gmail.com>
2019-04-06 00:22:25 -0700

    update user index

--- app/api.rb
+++ app/api.rb
@@ -7,4 +7,11 @@ class Book::BookApi
         data = open("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=RCGJYGTSwDbhvImLxnJ6VO1HyGNixAjm").read
         JSON.parse(data)
     end
+
+    def list_books
+        @books = Book.all
+        @books.each.with_index do |book, i|
+          puts "#{i+1}. #{book.title}"
+        end
+      end
 end
