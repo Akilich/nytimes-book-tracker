070a4a0a025249590e02315da55e9302c650715d
Akilich <akilihumphrey@gmail.com>
2019-04-10 10:28:31 -0700

    update api

--- app/book_api.rb
+++ app/book_api.rb
@@ -4,13 +4,12 @@ require 'json'
 
 class BookApi < ActiveRecord::Base
 
-    nyt_url = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key="
-    nyt_api_key = RCGJYGTSwDbhvImLxnJ6VO1HyGNixAjm
+    nyt_url = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=RCGJYGTSwDbhvImLxnJ6VO1HyGNixAjm"
     
     def get_books
         #data = open("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=RCGJYGTSwDbhvImLxnJ6VO1HyGNixAjm").read
         #JSON.parse(data)
-        data = JSON.parse(RestClient.get("#{nyt_url}#{ENV["nyt_api_key"]}"))
+        data = JSON.parse(RestClient.get("#{nyt_url}#{ENV["RCGJYGTSwDbhvImLxnJ6VO1HyGNixAjm"]}"))
     end 
 end
 
