9dbf10451933339d6819c985cd2ef126b30d0988
Akilich <akilihumphrey@gmail.com>
2019-04-10 10:29:24 -0700

    removed unnecessary module

--- app/book_module.rb
+++
@@ -1,8 +0,0 @@
-require_relative './app/book_api.rb'
-require_relative './app/models/book.rb'
-require_relative './app/controllers/users_controller.rb'
-
-module BestsellerBooks
-  class Error < StandardError; end
- 
-end
