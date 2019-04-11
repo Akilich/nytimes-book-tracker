6db03512782a93004fd9bb56205d6e9858a0ed67
Akilich <akilihumphrey@gmail.com>
2019-04-10 17:25:59 -0700

    update user controller and user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+
 class UsersController < ApplicationController
 
     get '/users/home' do
@@ -17,10 +18,7 @@ class UsersController < ApplicationController
 
     post '/user_books' do
     @book = Book.all
-    @book.each.with_index do |book, i|
-      puts "#{i+1}. #{book.title}"
       redirect "/users/user_books/:id"
-      end
     end
 
     get '/users/user_books/:id' do
@@ -35,28 +33,37 @@ class UsersController < ApplicationController
     end
 
     patch '/users/user_books/:id' do
-      @book = Book.find(params[:title])
+      @book = Book.find_by_id(params[:id])
       @user = User.find_by_id(params[:id])
       @username = params[:username]
       @book.save
       redirect "/user_books"
     end
+
+    delete '/users/user_books/:id/delete' do
+      if logged_in?
+        @book = Book.find_by_id(params[:id])
+          if @book && current_user
+              @book.destroy
+          end
+        redirect to '/users/home'
+        end
+    end
     
     get '/users/:id' do 
-      @user = User.find_by_id(params[:id])
+      @user = User.find_by_id(params[:username])
       @username = params[:username]
       erb :'users/#{@user.id}'
     end
 
     get '/users/:id/edit' do
-      @user = User.find(params[:id])
       @user = User.find_by(username: params[:username])
       erb :'/users/edit'
-  end
+    end
 
     patch '/users/:id' do 
-        @user = User.find_by_id(params[:id])
-        @username = params[:username]
+        @user = User.find_by(username: params[:username])
+        @username = (params[:username])
         @user.save
         redirect "users/home"
     end
